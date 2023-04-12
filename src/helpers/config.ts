7ee5390dfa7d349e8e5a204a965fe7e15aca42a5
Karasu <karasu.shing@gmail.com>
2023-04-12 11:03:34 +0800

    feat: add another way to set config

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -4,7 +4,9 @@ import os from 'os';
 import ini from 'ini';
 import type { TiktokenModel } from '@dqbd/tiktoken';
 import { commandName } from './constants';
-import { KnownError } from './error';
+import { KnownError, handleCliError } from './error';
+import * as p from '@clack/prompts';
+import { red } from 'kolorist';
 
 const { hasOwnProperty } = Object.prototype;
 export const hasOwn = (object: unknown, key: PropertyKey) =>
@@ -95,4 +97,68 @@ export const setConfigs = async (keyValues: [key: string, value: string][]) => {
   }
 
   await fs.writeFile(configPath, ini.stringify(config), 'utf8');
+};
+
+export const showConfigUI = async () => {
+  try {
+    const config = await getConfig();
+    const choice = (await p.select({
+      message: 'Set config:',
+      options: [
+        {
+          label: 'OpenAI Key',
+          value: 'OPENAI_KEY',
+          hint: hasOwn(config, 'OPENAI_KEY') ? config.OPENAI_KEY : '(not set)',
+        },
+        {
+          label: 'Silent Mode',
+          value: 'SILENT_MODE',
+          hint: hasOwn(config, 'SILENT_MODE')
+            ? config.SILENT_MODE.toString()
+            : '(not set)',
+        },
+        {
+          label: 'Model',
+          value: 'model',
+          hint: hasOwn(config, 'model') ? config.model : '(not set)',
+        },
+        {
+          label: 'Cancel',
+          value: 'cancel',
+          hint: 'Exit the program',
+        },
+      ],
+    })) as ConfigKeys | 'cancel' | symbol;
+
+    if (p.isCancel(choice)) return;
+
+    if (choice === 'OPENAI_KEY') {
+      const key = await p.text({
+        message: 'Enter your OpenAI API key',
+        validate: (value) => {
+          if (!value.startsWith('sk-')) return 'Must start with "sk-"';
+        },
+      });
+      if (p.isCancel(key)) return;
+      setConfigs([['OPENAI_KEY', key]]);
+    } else if (choice === 'SILENT_MODE') {
+      const silentMode = await p.confirm({
+        message: 'Enable silent mode?',
+      });
+      if (p.isCancel(silentMode)) return;
+      setConfigs([['SILENT_MODE', silentMode ? 'true' : 'false']]);
+    } else if (choice === 'model') {
+      const model = await p.text({
+        message: 'Enter the model you want to use',
+      });
+      if (p.isCancel(model)) return;
+      setConfigs([['model', model]]);
+    }
+    if (choice === 'cancel') return;
+    showConfigUI();
+  } catch (error: any) {
+    console.error(`\n${red('✖')} ${error.message}`);
+    handleCliError(error);
+    process.exit(1);
+  }
 };
