d02840e74f839a5d421b8f027dea1abf087e7f41
lizongliang <cqlzliang@qq.com>
2023-04-14 23:29:25 +0800

    FIX: fix the bug of steamToIterable

--- src/helpers/stream-to-iterable.ts
+++ src/helpers/stream-to-iterable.ts
@@ -1,7 +1,16 @@
 import { IncomingMessage } from 'http';
 
 export async function* streamToIterable(stream: IncomingMessage) {
+  let previous = "";
   for await (const chunk of stream) {
-    yield chunk;
+    const bufferChunk = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
+    previous += bufferChunk;
+    let eolIndex;
+    while ((eolIndex = previous.indexOf("\n")) >= 0) {
+      // line includes the EOL
+      const line = previous.slice(0, eolIndex + 1).trimEnd();
+      if (line.startsWith("data: ")) yield line;
+      previous = previous.slice(eolIndex + 1);
+    }
   }
 }
