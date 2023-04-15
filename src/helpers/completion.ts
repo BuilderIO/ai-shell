e89a3468bf4acf62ca9a4f0f2f9592fb31050bd7
HDKiller <hdkiller@gmail.com>
2023-04-15 15:20:40 +0200

    instruct the model instead of asking

--- src/helpers/completion.ts
+++ src/helpers/completion.ts
@@ -244,9 +244,7 @@ const shellDetails = dedent`
 `;
 
 const explainScript = dedent`
-  Then please describe the script in plain english, step by step, what exactly it does.
-  Please describe succinctly, use as few words as possible, do not be verbose.
-  If there are multiple steps, please display them as a list.
+  Please provide a clear, concise description of the script, using minimal words. Outline the steps in a list format.
 `;
 
 function getOsType() {
@@ -255,15 +253,15 @@ function getOsType() {
 }
 
 const generationDetails = dedent`
-  Please only reply with the single line command surrounded by 3 backticks. It should be able to be directly run in a terminal. Do not include any other text.
+  Only reply with the single line command surrounded by 3 backticks. It should be able to be directly run in a terminal. Do not include any other text.
 
-  Please make sure the script runs on ${getOsType()} operating system.
+  Make sure the script runs on ${getOsType()} operating system.
 `;
 
 // TODO: gather the current OS (Windows, Mac, Linux) and add that to the prompt that it should support this system.
 function getFullPrompt(prompt: string) {
   return dedent`
-    I will give you a prompt to create a single line command that one can enter in a terminal and run, based on what is asked in the prompt.
+    Create a single line command that one can enter in a terminal and run, based on what is asked in the prompt.
 
     ${shellDetails}
 
@@ -277,7 +275,7 @@ function getFullPrompt(prompt: string) {
 
 function getRevisionPrompt(prompt: string, code: string) {
   return dedent`
-    Please update the following script based on what is asked in the following prompt.
+    Update the following script based on what is asked in the following prompt.
 
     The script: ${code}
 
