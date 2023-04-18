7d40a8e842807aae3f34f041725c6774e41cc14a
Jong Hyeon Yeo <37054916+mindgitrwx@users.noreply.github.com>
2023-04-18 14:21:02 +0900

    Update copy hint
    Co-authored-by: Steve Sewell <steve@builder.io>

--- src/prompt.ts
+++ src/prompt.ts
@@ -163,7 +163,7 @@ async function runOrReviseFlow(
         value: 'revise',
         hint: 'Give feedback via prompt and get a new result',
       },
-      { label: '📋 Copy', value: 'copy', hint: 'Copy the generated script' },
+      { label: '📋 Copy', value: 'copy', hint: 'Copy the generated script to your clipboard' },
       { label: '❌ Cancel', value: 'cancel', hint: 'Exit the program' },
     ],
   });
